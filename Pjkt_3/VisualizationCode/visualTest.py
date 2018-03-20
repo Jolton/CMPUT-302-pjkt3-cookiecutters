24cbbf46812132d1bf029f1acca335e09d8cbfe3
Jolton <chrisrwalter@me.com>
2018-03-20 11:20:25 -0600

    test ui using tkinter

--- Pjkt_3/VisualizationCode/visualTest.py
+++ Pjkt_3/VisualizationCode/visualTest.py
@@ -6,15 +6,32 @@ import tkinter
 
 
 
-# domains = DataParser.parseTables()
+domains = DataParser.parseTables()
 
+
+class App:
+
+    def __init__(self, master, data:[DataStructures.Domain]):
+        frame = tkinter.Frame(master)
+        frame.pack()
+
+        self.buttons = []
+        for domain in data:
+            button = tkinter.Button(frame, text=domain.name, command= lambda domain=domain : self.printDomain(domain))
+            button.pack(side=tkinter.TOP)
+            self.buttons.append(button)
+
+    def printDomain(self, domain:DataStructures.Domain):
+        print("Domain name: " + domain.name)
+
+        for library in domain.libraries:
+            print("Domain contains library: " + library.name)
 
 
 
 root = tkinter.Tk()
 
-w = tkinter.Label(root, text="Hello World")
-w.pack()
+app = App(root, domains)
 
 root.mainloop()
 
